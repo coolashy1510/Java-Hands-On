d763bc2e0e65b1ee1d9b63f71d366ae9d79fe851
Ashwin Kumar <coolashy1510@gmail.com>
2024-06-12 06:18:07 +0000

    Loop Challenges V1

---
+++ ForChallenge/DigitSumChallenge.java
@@ -0,0 +1,27 @@
+package com.io.Java11.ForChallenge;
+
+public class DigitSumChallenge {
+    public static void main(String[] args) {
+       System.out.println( "The sum of Digits is " + sumDigits(1234));
+        System.out.println( "The sum of Digits is " + sumDigits(-125));
+        System.out.println( "The sum of Digits is " + sumDigits(4));
+        System.out.println( "The sum of Digits is " + sumDigits(32123));
+    }
+
+    public static int sumDigits (int number) {
+
+        if ( number < 0 ) {
+            return -1;
+        }
+
+        int sum =0;
+
+        while (number > 9) {
+            sum += (number %10);
+            number = number / 10;
+        }
+        sum += number;
+
+        return sum;
+    }
+}
