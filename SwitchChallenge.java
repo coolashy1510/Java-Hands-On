d0454b6282c131ba22828fe7762b4e52c5857db6
Ashwin Kumar <coolashy1510@gmail.com>
2024-06-12 06:15:27 +0000

    Switch - Java 17

---
+++ SwitchChallenge.java
@@ -0,0 +1,19 @@
+package com.io.Java11;
+
+public class SwitchChallenge {
+    public static void main(String[] args) {
+       System.out.println(printDayOFWeek(9));
+    }
+    public static String printDayOFWeek (int day) {
+        return switch (day) {
+            case 0 -> "Monday";
+            case 1 -> "Tuesday";
+            case 2 -> "Wednesday";
+            case 3 -> "Thursday";
+            case 4 -> "Friday";
+            case 5 -> "Saturday";
+            case 6 -> "Sunday";
+            default -> "Invalid Day";
+        };
+    }
+}
