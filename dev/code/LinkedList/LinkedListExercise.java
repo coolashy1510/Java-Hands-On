ce69fa898f18aa8d03cac5f6c9de81a85bf1fa19
Ashwin Kumar <coolashy1510@gmail.com>
2025-01-05 10:40:10 +0000

    Linkedlist exercise

--- dev/code/LinkedList/LinkedListExercise.java
+++ dev/code/LinkedList/LinkedListExercise.java
@@ -10,8 +10,12 @@ public class LinkedListExercise {
         linkedList.add("Mumbai");
         linkedList.add("Kolkata");
         System.out.println(linkedList);
+
         addElements(linkedList);
-        System.out.println(linkedList);
+        System.out.println("Added Elements: " + linkedList);
+
+        removeElements(linkedList);
+        System.out.println("Removed Elements: " + linkedList);
     }
 
     private static void addElements(LinkedList<String> linkedList) {
@@ -29,5 +33,42 @@ public class LinkedListExercise {
         //Stack Methods
         linkedList.push("Hyderabad"); // adds element to the first index
 
+    }
+
+    private static void removeElements(LinkedList<String> linkedList) {
+
+        linkedList.remove("Kolkata"); // removes the element
+
+        String s1 = linkedList.remove(); // removes the first element
+        System.out.println(s1 + " removed from the list");
+        String s2 = linkedList.removeFirst(); // removes the first element
+        System.out.println(s2 + " removed from the list");
+        String s3 = linkedList.removeLast(); // removes the last element
+        System.out.println(s3 + " removed from the list");
+        String s4 = linkedList.remove(5); // removes the element at index 5
+        System.out.println(s4 + " removed from the list");
+
+        System.out.println(linkedList);
+
+        // Queue methods
+        String s5 = linkedList.poll(); // removes the first element
+        System.out.println(s5 + " removed from the list");
+
+        String s6 = linkedList.pollFirst(); // removes the first element
+        System.out.println(s6 + " removed from the list");
+
+        String s7 = linkedList.pollLast(); // removes the last element
+        System.out.println(s7 + " removed from the list");
+
+        linkedList.push("Pondicherry");
+        linkedList.push("Goa");
+        System.out.println(linkedList);
+
+        // Stack methods
+        String s8 = linkedList.pop(); // removes the first element
+        System.out.println(s8 + " removed from the list");
+
+        boolean b = linkedList.removeFirstOccurrence("Mumbai"); // removes the first occurrence
+        System.out.println(b);
     }
 }
